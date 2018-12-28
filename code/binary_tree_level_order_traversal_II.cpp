611d43846d9de5df3b70b9425861ce775bcf1f29
Yyaw <1947302673@qq.com>
2018-12-28 19:04:38 +0800

    modified:   code/binary_tree_level_order_traversal_II.cpp

--- code/binary_tree_level_order_traversal_II.cpp
+++ code/binary_tree_level_order_traversal_II.cpp
@@ -7,13 +7,38 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-	queue<TreeNode *> q1, q2;
-	q1.push(root);
-	while (!q1.empty() && !q2.empty()) {
-	if (!q1.empty()) {
-	    }else{
-	    }
-	}
+        vector<vector<int>> result;
+        queue<TreeNode *> q1, q2;
+        q1.push(root);
+        while (!q1.empty() || !q2.empty()) {
+            if (!q1.empty()) {
+                vector<int> r;
+                while (!q1.empty()) {
+                    r.push_back(q1.front()->val);
+                    if (q1.front()->left != NULL) {
+                        q2.push_back(q1.front()->left);
+                    }
+                    if (q1.front()->right != NULL) {
+                        q2.push_back(q1.front()->right);
+                    }
+                    q1.pop();
+                }
+            }else {
+                vector<int> r;
+                while (!q2.empty()) {
+                    r.push_back(q2.front()->val);
+                    if (q2.front()->left != NULL) {
+                        q1.push_back(q2.front()->left);
+                    }
+                    if (q2.front()->right != NULL) {
+                        q1.push_back(q2.front()->right);
+                    }
+                    q2.pop();
+                }
+            }
+            result.push_back(q2);
+        }
+        return result;
     }   
 };
 
@@ -22,3 +47,8 @@ int main(int argc, char **argv)
     Solution s;
     return 0;
 }
+
+
+
+
+
